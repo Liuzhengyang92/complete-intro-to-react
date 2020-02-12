cb65b9db8abd12ca04fea3f32ccd1ee5abb513f1
felixatchur <felix@churinc.com>
2020-02-13 09:08:59 +1100

    use async function

---
+++ adopt-me/src/SearchParams.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
+import useDropdown from "./useDropdown";
+
+const SearchParams = () => {
+  const [location, setLoation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown, setAnimal] = useDropdown(
+    "Animal",
+    "dog",
+    ANIMALS
+  );
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    console.log("animals", animals);
+
+    setPets(animals || []);
+  }
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(
+      ({ breeds }) => {
+        const breedStrings = breeds.map(({ name }) => name);
+        setBreeds(breedStrings);
+      }
+      // error => console.error
+    );
+  }, [animal, setBreed, setBreeds]); //if given the empty array, the useEffect will only
+  //run once. It will never run again.
+
+  return (
+    <div className="search-params">
+      <h1>{location}</h1>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
+        <label htmlFor="location">
+          location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={event => setLoation(event.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+      <Results pets={pets} />
+    </div>
+  );
+};
+
+export default SearchParams;
